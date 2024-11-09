89da72b27a52495175a3bff77cacfc6c41914c29
Gryast <mcbroom3.14@gmail.com>
2024-11-09 12:12:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
+# https://github.com/stanfordnlp/stanza/blob/main/demo/Stanza_Beginners_Guide.ipynb
+
+# Install; note that the prefix "!" is not needed if you are running in a terminal
+!pip install stanza
+
+# Import the package
+import stanza
+# Download an English model into the default directory
+print("Downloading English model...")
+stanza.download('en')
+
 print ("Three little boys")
 print('lAPJF n')
