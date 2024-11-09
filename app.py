c7320de6b546167f03738a51a0ec39c18eb03c5c
Gryast <mcbroom3.14@gmail.com>
2024-11-09 12:24:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 # https://github.com/stanfordnlp/stanza/blob/main/demo/Stanza_Beginners_Guide.ipynb
 
-# Install; note that the prefix "!" is not needed if you are running in a terminal
-!pip install stanza
-
-# Import the package
 import stanza
-# Download an English model into the default directory
-print("Downloading English model...")
-stanza.download('en')
+>>> stanza.download('en') # download English model
+>>> nlp = stanza.Pipeline('en') # initialize English neural pipeline
+>>> doc = nlp("Barack Obama was born in Hawaii.") # run annotation over a sentence
 
 print ("Three little boys")
 print('lAPJF n')
